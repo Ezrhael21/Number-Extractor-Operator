ebd9581aef72b1b5a9438123b936dcdef358ab62
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-28 01:46:53 +0800

    Pseudocode

---
+++ Number_Extractor_Operator.py
@@ -0,0 +1,18 @@
+# Ezrhael R. Sicat
+# BSCpE 1-5
+# 04/25/2023
+# Write a method in python that will create two separate text files after reading 
+# the source text file named integers.txt that contains 20 integers. The first 
+# output file will be named double.txt containing the square of all even integers 
+# found in integers.txt and the second file will be named triple.txt containing the 
+# cube of all odd numbers found in the integers.txt.
+
+# ask user for input
+# write the numbers to the file
+# read the contents of the file
+# loop through each number and add it to the appropriate list
+# square the even numbers
+# cube the odd numbers
+# convert the list into string
+# display output
+# write odd and even numbers to files
